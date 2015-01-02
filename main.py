93f0edaa852f8c302aea41afdfd4cb21eaa736ad
Kevin Tan <kevin.tan@yale.edu>
2015-01-02 15:05:21 -0500

    db url

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 from flask.ext.socketio import SocketIO
 
+#ignore (allow you to print)
+import logging
+from logging import StreamHandler
+file_handler = StreamHandler()
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
+
 # Set up app with debugging
 app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app)
 thread = None
 
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    
+app.logger.debug(SQLALCHEMY_DATABASE_URI)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['BUTTERY_DATABASE_URL']
 db = SQLAlchemy(app)
 
 # for flask-login -- secret key needed to use sessions
